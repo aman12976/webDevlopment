43634db178858aaec72c48816a33d0d612d1e464
aman12976 <amanwaghade2005@gmail.com>
2026-04-29 19:10:36 +0530

    js loop practice

--- JSBasic/script.js
+++ JSBasic/script.js
@@ -17,3 +17,11 @@ for(var i=1; i<=10; i++)
         
         console.log(i*3);
     }
+
+
+    let a= [24,58,33,86,96,12,45,78,90,11];
+    for(let i=0; i<10; i++)
+    {
+        console.log(a[i]);
+    }
+
