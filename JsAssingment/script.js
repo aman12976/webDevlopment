575070a3fd378051da301694f1b684c14edc2d34
aman12976 <amanwaghade2005@gmail.com>
2026-04-30 18:15:14 +0530

    js loop practice

---
+++ JsAssingment/script.js
@@ -0,0 +1,71 @@
+// question 5
+
+// console.log("aman waghade")
+
+
+function aman(){
+    console.log("aman waghade")
+}
+
+for(var i=1; i<=5; i++)
+{
+    console.log(i);
+}
+for(var i=1; i<=10; i++)
+{
+    console.log(i);
+}
+
+
+for(var i=10; i>=1; i--)
+{
+    console.log(i);
+}
+
+for(var i=1; i<=5; i++)
+{
+    console.log(i*2);
+}
+
+for(var i=1; i<=10; i++)
+{
+    console.log(i*15);
+}
+for(var i=1; i<=20; i += 2)
+{
+    console.log(i);
+}
+
+for(var i=5; i<=50; i+= 5)
+{
+    console.log(i);
+}
+
+for(var i=1; i<=10; i++)
+{
+    console.log(i*i);
+}
+for(var i=1; i<=10; i++)
+{
+    if(i%3==0){
+        console.log(i);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// while loop
+
+let i =1;
+while(i<=5)
+{
+    console.log(i);
+    i++;
+}
