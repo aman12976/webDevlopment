9b0a3272e75daf9d75f827ae2aeaad20095a6fdf
aman12976 <amanwaghade2005@gmail.com>
2026-05-01 19:18:11 +0530

    changes in function code

--- JSMethod/script.js
+++ JSMethod/script.js
@@ -1,17 +1,26 @@
-let a = 20; b=30;
+let a = 20;
+b = 30;
 
-function sum (x,y){
-    let z= b+a;
-    console.log(z);
+function sum(x, y) {
+  let z = b + a;
+  console.log(z);
 }
-sum(a,b)
+sum(a, b);
 
-
-
-
-let abc = function(){
-    console.log("hello world ");
-    
-}
+let abc = function () {
+  console.log("hello world ");
+};
 
 console.log(abc);
+
+abc();
+
+
+
+let add = function(a, b) {
+    return a + b;
+};
+
+console.log(add);
+
+console.log(add(4, 6));
