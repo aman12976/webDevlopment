189df2d9542bab1a8a390161ab3ab272015e7aca
aman12976 <amanwaghade2005@gmail.com>
2026-05-05 18:27:19 +0530

    create a new page with the help of html css js

--- JSDom/script.js
+++ JSDom/script.js
@@ -1,9 +1,29 @@
-function Submit() {
-  console.log("Submit button clicked");
+// function Submit() {
+//   console.log("Submit button clicked");
 
+//   const fn = document.getElementById("fullName").value;
+//   console.log(fn);
+//   document.getElementById("myData").innerText =fn;
+//    document.getElementById("fullName").value = " ";
+
+// }
+
+
+
+function Submit() {
+  console.log("Submit Button Click");
   const fn = document.getElementById("fullName").value;
+  const City = document.getElementById("city").value;
+  const Email = document.getElementById("email").value;
+  const Passward = document.getElementById("Passward").value;
+
   console.log(fn);
   document.getElementById("myData").innerText = fn;
-   document.getElementById("fullName").value = " ";
+  document.getElementById("myData2").innerText = City;
+  document.getElementById("myData3").innerText = Email;
+  document.getElementById("myData4").innerText = Passward;
+  document.getElementById("myData").value = " ";
 
+  document.getElementById("dataCard").classList.add("divShow");
+   document.getElementById("dataCard").classList.remove("divHide");
 }
