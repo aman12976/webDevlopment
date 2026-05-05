4378ad113a08e7f471466d5abcebcef76c665b5e
aman12976 <amanwaghade2005@gmail.com>
2026-05-05 19:33:26 +0530

    Array practice in js

---
+++ JSArray/script.js
@@ -0,0 +1,35 @@
+let arr = [10,26,45,68,12,31,2,24]
+
+// for(var i=0; i<8; i++)
+// {
+//     console.log(arr[i]);
+    
+// }
+// console.log(arr);
+
+// arr[2]= arr[2]+10;
+// console.log(arr);
+
+// arr = [];
+// console.log(arr);
+
+
+arr.pop();
+console.log(arr);
+
+arr.push(58);
+console.log(arr);
+
+arr.unshift(11)
+console.log(arr);
+arr.shift(12);
+console.log(arr);
+
+console.log(arr.at(6));
+
+
+
+
+
+
+
