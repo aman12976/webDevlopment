37e9e4dea96288c22d2095fc2bf9382b1d778381
aman12976 <amanwaghade2005@gmail.com>
2026-05-06 19:29:10 +0530

    keys and value practice

--- JSArray/script.js
+++ JSArray/script.js
@@ -14,96 +14,109 @@ let arr = [10,26,45,68,12,31,2,24]
 // console.log(arr);
 
 
-arr.pop();
-console.log(arr);
+// arr.pop();
+// console.log(arr);
 
-arr.push(58);
-console.log(arr);
+// arr.push(58);
+// console.log(arr);
 
-arr.unshift(11)
-console.log(arr);
-arr.shift(12);
-console.log(arr);
+// arr.unshift(11)
+// console.log(arr);
+// arr.shift(12);
+// console.log(arr);
 
-console.log(arr.at(6));
-
-
-
-let ar = [10,26,45,68,12,31,2,24]
-
-for(var i=0; i<8; i++)
-{     console.log(ar[i]);
-
-
-}
-console.log("for each loop");
-
-ar.forEach((Element)=>{console.log(Element);
-});
+// console.log(arr.at(6));
 
 
 
+// let ar = [10,26,45,68,12,31,2,24]
+
+// for(var i=0; i<8; i++)
+// {     console.log(ar[i]);
 
 
-let arr1 = [];
-for(var i = 1; i<2;i++)
-{
-    console.log(arr1);
-}
+// }
+// console.log("for each loop");
 
-arr1.push(15);
-console.log(arr1);
+// ar.forEach((Element)=>{console.log(Element);
+// });
 
 
 
-let fn =[
-  {
+
+
+// let arr1 = [];
+// for(var i = 1; i<2;i++)
+// {
+//     console.log(arr1);
+// }
+
+// arr1.push(15);
+// console.log(arr1);
+
+
+
+// let fn =[
+//   {
+//     "name": "Aman Verma",
+//     "dob": "2002-05-14",
+//     "mobile": "9876543210",
+//     "city": "Bhopal",
+//     "job": "Software Developer"
+//   },
+//   {
+//     "name": "Rahul Sharma",
+//     "dob": "2001-11-22",
+//     "mobile": "9123456780",
+//     "city": "Indore",
+//     "job": "Data Analyst"
+//   },
+//   {
+//     "name": "Neha Singh",
+//     "dob": "2003-07-09",
+//     "mobile": "9988776655",
+//     "city": "Delhi",
+//     "job": "UI/UX Designer"
+//   },
+//   {
+//     "name": "Priya Patel",
+//     "dob": "2000-03-18",
+//     "mobile": "9090909090",
+//     "city": "Ahmedabad",
+//     "job": "Digital Marketer"
+//   },
+//   {
+//     "name": "Rohit Gupta",
+//     "dob": "2002-12-30",
+//     "mobile": "9012345678",
+//     "city": "Jaipur",
+//     "job": "Mechanical Engineer"
+//   }
+// ]
+
+// fn.forEach(item => {
+//     console.log(item["job"]);
+    
+// });
+ 
+
+// console.log(fn);
+// console.log(fn[1]["name"]);
+// console.log(fn[2][""]);
+
+
+
+
+let aman ={
     "name": "Aman Verma",
     "dob": "2002-05-14",
     "mobile": "9876543210",
     "city": "Bhopal",
     "job": "Software Developer"
-  },
-  {
-    "name": "Rahul Sharma",
-    "dob": "2001-11-22",
-    "mobile": "9123456780",
-    "city": "Indore",
-    "job": "Data Analyst"
-  },
-  {
-    "name": "Neha Singh",
-    "dob": "2003-07-09",
-    "mobile": "9988776655",
-    "city": "Delhi",
-    "job": "UI/UX Designer"
-  },
-  {
-    "name": "Priya Patel",
-    "dob": "2000-03-18",
-    "mobile": "9090909090",
-    "city": "Ahmedabad",
-    "job": "Digital Marketer"
-  },
-  {
-    "name": "Rohit Gupta",
-    "dob": "2002-12-30",
-    "mobile": "9012345678",
-    "city": "Jaipur",
-    "job": "Mechanical Engineer"
   }
-]
 
-fn.forEach(item => {
-    console.log(item["job"]);
-    
-});
- 
-
-console.log(fn);
-console.log(fn[1]["name"]);
-console.log(fn[2][""]);
-
+console.log(Object.keys(aman));
+console.log(Object.values(aman));
 
 
 
